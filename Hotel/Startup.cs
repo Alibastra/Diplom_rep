2a2b5d8465ccbd097f47d4ae1f55ac7202f8e4d1
Анастасия Уварова <kotenok26.97@mail.ru>
2019-03-30 20:22:00 +0300

    begin

--- Hotel/Startup.cs
+++ Hotel/Startup.cs
@@ -9,26 +9,22 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Hotel
 {
-    public class Startup
+    public class Startup  //см главу 14
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage(); 
+            app.UseStatusCodePages();//Расширение ответов об ошибках
+            app.UseStaticFiles();//обслуживание стратического содержимого из папки wwwroot
+            app.UseMvcWithDefaultRoute();//инраструктура ASP.NET Core MVC
         }
     }
 }
