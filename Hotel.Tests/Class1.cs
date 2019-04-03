d4d484a9ec451cc1eaf2b4be24e94057876e8c39
Анастасия Уварова <kotenok26.97@mail.ru>
2019-04-03 22:33:26 +0300

    03.04

--- Hotel.Tests/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Hotel.Tests
-{
-    public class Class1
-    {
-    }
-}
--- Hotel/Controllers/RoomController.cs
+++ Hotel/Controllers/RoomController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Hotel.Models;
-
+using System.Linq;
 
 namespace Hotel.Controllers
 {
     public class RoomController:Controller
     {
+        public int PageSize = 4; 
         private IRoomRepository repository;
         public RoomController (IRoomRepository repo)
         {
             repository = repo;
         }
-        public ViewResult List() => View(repository.Rooms);
+        public ViewResult List(int page=1) 
+            => View(repository.Rooms
+                .OrderBy(p=>p.RoomID)
+                .Skip((page-1)*PageSize)
+                .Take(PageSize));
     }
 }
